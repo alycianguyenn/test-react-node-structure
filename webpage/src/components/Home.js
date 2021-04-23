73cb6085c9c8fb64a5ca9c6b3ce06cf303fdf996
Alycia <alycin2@uw.edu>
2021-04-23 00:15:34 -0700

    Add yes and no buttons

--- webpage/src/components/Home.js
+++ webpage/src/components/Home.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useSound from 'use-sound';
+
+// for test purposes 
+import { Button } from 'reactstrap';
 
 // importing files
 import tone1 from './440Hz_5sec_test.mp3';
 
-const Contact = () => {
+const PlayAudioButton = () => {
     return (
-        <div className="main-content">
-            <h1>Hearing Screening</h1>
+        <div>
+            <p>Button to Play Tone</p>
             <ToneButton></ToneButton>
         </div>
     );
@@ -17,16 +20,30 @@ const Contact = () => {
 const ToneButton = () => {
     const [play] = useSound(tone1);
     return (
-    <button onClick={play}>Play Tone</button>
+        <p><button onClick={play}>Play Tone</button></p>
     );
 }
+
+// yes or no button handler stuff
+const YesNoButtons = (props) => {
+    const [rSelected, setRSelected] = useState(null); // radio button stuff
+    return (
+        <div>
+            <Button color="success" onClick={() => setRSelected("Yes")} active={rSelected === "Yes"}>Yes!</Button>{' '}
+            <Button color="danger" onClick={() => setRSelected("No")} active={rSelected === "No"}> Nope!</Button>{' '}
+            <p>Selected: {rSelected}</p>
+       </div>
+    );
+
+};
 
 const home = () => {
     return (
        <main>
           <h1>Home</h1>
            <p>Put hearing all groups landing page here</p>
-           <Contact></Contact>
+           <PlayAudioButton />
+           <YesNoButtons />
        </main>
     );
 }
