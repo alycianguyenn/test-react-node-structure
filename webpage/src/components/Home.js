efa78651a70f06c7e3179bf94e51f92390bd6ab6
Alycia <alycin2@uw.edu>
2021-04-22 23:25:54 -0700

    Add audio player button and an audio file. Placed in temp place until hearing test pages are ready

--- webpage/src/components/Home.js
+++ webpage/src/components/Home.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import useSound from 'use-sound';
+
+// importing files
+import tone1 from './440Hz_5sec_test.mp3';
+
+const Contact = () => {
+    return (
+        <div className="main-content">
+            <h1>Hearing Screening</h1>
+            <ToneButton></ToneButton>
+        </div>
+    );
+   }
+    
+// ToneButton is called above as an HTML element.
+const ToneButton = () => {
+    const [play] = useSound(tone1);
+    return (
+    <button onClick={play}>Play Tone</button>
+    );
+}
 
 const home = () => {
     return (
        <main>
           <h1>Home</h1>
            <p>Put hearing all groups landing page here</p>
+           <Contact></Contact>
        </main>
     );
 }
